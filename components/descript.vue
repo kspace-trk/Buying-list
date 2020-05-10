99ebbdb7f91d75b7244ca17eeb679cdeff6d299a
中尾圭吾 <knakao@nakaokeigonoiMac.local>
2020-05-11 03:04:08 +0900

    [add]名前ボタン追加

--- components/descript.vue
+++ components/descript.vue
@@ -3,9 +3,25 @@
     <div class="container">
       <input class="name" placeholder="買ってきて欲しいもの" />
       <textarea class="descript" placeholder="どんなやつ？" />
+      <div class="who-name">
+        <p>だれ？：{{nakao}}</p>
+        <div class="who-wrapper">
+          <button v-on:click="nakao = むねき" class="who">むねき</button>
+          <button v-on:click="nakao = まきこ" class="who" value="まきこ">まきこ</button>
+          <button v-on:click="nakao = あかり" class="who" value="あかり">あかり</button>
+          <button v-on:click="nakao = けいご" class="who" value="けいご">けいご</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data: {
+    nakao: ""
+  }
+};
+</script>
 <style scoped>
 .container {
   display: flex;
@@ -39,5 +55,16 @@
   font-weight: 600;
   border: 2px solid #d7d7d7;
   color: #404040;
+}
+.who-wrapper {
+  display: flex;
+  padding-top: 0.5rem;
+}
+.who {
+  width: 5.2rem;
+  height: 4rem;
+  border-radius: 10px;
+  background-color: #ffffff;
+  border: 2px solid #d7d7d7;
 }
 </style>
