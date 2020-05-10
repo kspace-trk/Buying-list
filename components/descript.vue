ba1e920921a028ee949266e6770d90963ed04ce0
中尾圭吾 <knakao@nakaokeigonoiMac.local>
2020-05-11 02:01:39 +0900

    [add]追加フォーム

--- components/descript.vue
+++ components/descript.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
     <div class="container">
-      <p>ああああああああああ</p>
+      <input class="name" placeholder="買ってきて欲しいもの" />
     </div>
   </div>
 </template>
 <style scoped>
 .container {
-  padding-top: 100px;
+  display: flex;
+  justify-content: center;
+}
+.name {
+  width: 90%;
+  height: 3.5rem;
+  background-color: #ffffff;
+  margin-top: 130px;
+  padding-left: 1rem;
+  border-radius: 10px;
+  font-size: 1.5rem;
+  font-weight: 600;
+  border: 2px solid #d7d7d7;
+  color: #404040;
+}
+::placeholder {
+  color: #b0b0b0;
 }
 </style>
